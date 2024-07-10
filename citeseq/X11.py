fixing error of Multiome (float32 not enough) and calculation of clustering

citeseq/X11.py
@@ -24,7 +24,7 @@ from scipy.stats import pearsonr
 epochs = 30000
 device = 'cuda:0'
 
-citeseq = anndata.read_h5ad("./../data/GSE194122_openproblems_neurips2021_cite_BMMC_processed.h5ad")
+citeseq = anndata.read_h5ad("/workspace/ImputationOT/data/GSE194122_openproblems_neurips2021_cite_BMMC_processed.h5ad")
 citeseq.var_names_make_unique()
 
 ### preprocess
@@ -68,52 +68,75 @@ optimizer = optim.Adam([imps], lr=0.1)
 lambda_lr = lambda epoch: 1 if epoch < 1000 else 0.001 + (0.1 - 0.001) * (1 - (epoch - 1000) / (epochs - 1000))
 scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_lr)
 
-def perform_kmeans_clustering_and_evaluate(anndata, modality1='GEX', modality2='ADT', n_clusters=45):
-    """
-    Perform K-means clustering on UMAP embeddings of specified modalities of an AnnData object 
-    and evaluate with ARI and NMI.
-    
-    Parameters:
-    anndata (AnnData): AnnData object containing the multiome data.
-    modality1 (str): The first modality to include in clustering (default is 'GEX').
-    modality2 (str): The second modality to include in clustering (default is 'ADT').
-    n_clusters (int): Number of clusters for K-means.
-    
-    Returns:
-    None
-    """
-    anndata = anndata.copy()
-    
-    if f'{modality1}_X_umap' not in anndata.obsm.keys() or f'{modality2}_X_umap' not in anndata.obsm.keys():
-        sc.pp.neighbors(anndata, use_rep='X')
-        sc.tl.umap(anndata)
-        
-    X = np.hstack([anndata.obsm[f'{modality1}_X_umap'], anndata.obsm[f'{modality2}_X_umap']])
-    anndata.obsm['X_umap'] = X
-    print(f"Shape of the combined UMAP matrix: {X.shape}")
-    
-    # Perform K-means clustering
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-    clusters = kmeans.fit_predict(X)
-    anndata.obs['kmeans_clusters'] = clusters
-    
-    # Optionally, print out the first few rows of the obs attribute to check the new column
-    print(anndata.obs.head())
-    
-    ground_truth_labels = anndata.obs['cell_type']
-    predicted_clusters = anndata.obs['kmeans_clusters']
-    
-    ari = adjusted_rand_score(ground_truth_labels, predicted_clusters)
-    nmi = normalized_mutual_info_score(ground_truth_labels, predicted_clusters)
-    
-    print(f"Adjusted Rand Index (ARI): {ari}")
-    print(f"Normalized Mutual Information (NMI): {nmi}")
-    
-    sc.pl.umap(anndata, color='kmeans_clusters', save='_kmeans_clusters_umap.png')
-    plt.savefig('kmeans_clusters_umap.png')
-    print("UMAP plot saved as 'kmeans_clusters_umap.png'")
+def clustering(
+    adata,
+    resolutions,
+    clustering_method,
+    cell_type_col,
+    batch_col,
+):
+    """Clusters the data and calculate agreement with cell type and batch
+    variable.
 
-perform_kmeans_clustering_and_evaluate(citeseq)
+    This method cluster the neighborhood graph (requires having run sc.pp.
+    neighbors first) with "clustering_method" algorithm multiple times with the
+    given resolutions, and return the best result in terms of ARI with cell
+    type.
+    Other metrics such as NMI with cell type, ARi with batch are logged but not
+    returned. (TODO: also return these metrics)
+
+    Args:
+        adata: the dataset to be clustered. adata.obsp shouhld contain the keys
+            'connectivities' and 'distances'.
+        resolutions: a list of leiden/louvain resolution parameters. Will
+            cluster with each resolution in the list and return the best result
+            (in terms of ARI with cell type).
+        clustering_method: Either "leiden" or "louvain".
+        cell_type_col: a key in adata.obs to the cell type column.
+        batch_col: a key in adata.obs to the batch column.
+
+    Returns:
+        best_cluster_key: a key in adata.obs to the best (in terms of ARI with
+            cell type) cluster assignment column.
+        best_ari: the best ARI with cell type.
+        best_nmi: the best NMI with cell type.
+    """
+
+    assert len(resolutions) > 0, f"Must specify at least one resolution."
+
+    if clustering_method == "leiden":
+        clustering_func = sc.tl.leiden
+    elif clustering_method == "louvain":
+        clustering_func = sc.tl.louvain
+    else:
+        raise ValueError(
+            "Please specify louvain or leiden for the clustering method argument."
+        )
+        
+    assert cell_type_col in adata.obs, f"{cell_type_col} not in adata.obs"
+    best_res, best_ari, best_nmi = None, -inf, -inf
+    for res in resolutions:
+        col = f"{clustering_method}_{res}"
+        clustering_func(adata, resolution=res, key_added=col)
+        ari = adjusted_rand_score(adata.obs[cell_type_col], adata.obs[col])
+        nmi = normalized_mutual_info_score(adata.obs[cell_type_col], adata.obs[col])
+        n_unique = adata.obs[col].nunique()
+        if ari > best_ari:
+            best_res = res
+            best_ari = ari
+        if nmi > best_nmi:
+            best_nmi = nmi
+        if batch_col in adata.obs and adata.obs[batch_col].nunique() > 1:
+            ari_batch = adjusted_rand_score(adata.obs[batch_col], adata.obs[col])
+            # print(f'Resolution: {res:5.3g}\tARI: {ari:7.4f}\tNMI: {nmi:7.4f}\tbARI: {ari_batch:7.4f}\t# labels: {n_unique}')
+        else:
+            # print(f'Resolution: {res:5.3g}\tARI: {ari:7.4f}\tNMI: {nmi:7.4f}\t# labels: {n_unique}')
+            a = None
+
+    return f"{clustering_method}_{best_res}", best_ari, best_nmi
+
+scanpy.pp.neighbors(citeseq)
+print(clustering(citeseq, np.arange(0.75, 2, 0.1)))
 sys.exit()
 
 print("start optimizing")

multiome/X32.py
@@ -6,7 +6,11 @@ import ot
 import sys
 import anndata
 import scanpy as sc
-# import wandb
+import wandb
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 from scipy.stats import pearsonr
 
 # wandb.init(
@@ -25,14 +29,15 @@ multiome = anndata.read_h5ad("/workspace/ImputationOT/data/GSE194122_openproblem
 multiome.var_names_make_unique()
 
 ### preprocess
-### step 1
+#####################################################################################################################################
+### step 1: normalize
 adata_GEX = multiome[:, multiome.var['feature_types'] == 'GEX'].copy()
 adata_ATAC = multiome[:, multiome.var['feature_types'] == 'ATAC'].copy()
 sc.pp.normalize_total(adata_GEX, target_sum=1e4)
 sc.pp.normalize_total(adata_ATAC, target_sum=1e4)
-multiome.X[:, multiome.var['feature_types'] == 'GEX'] = adata_GEX.X
-multiome.X[:, multiome.var['feature_types'] == 'ATAC'] = adata_ATAC.X
-### step 2
+citeseq.X[:, multiome.var['feature_types'] == 'GEX'] = adata_GEX.X
+citeseq.X[:, multiome.var['feature_types'] == 'ATAC'] = adata_ATAC.X
+### step 2: log transform
 sc.pp.log1p(multiome)
 ### step 3
 adata_GEX = multiome[:, multiome.var['feature_types'] == 'GEX'].copy()
@@ -41,37 +46,37 @@ sc.pp.highly_variable_genes(
     n_top_genes=2000,
     subset=True
 )
-adata_ADAC = multiome[:, multiome.var['feature_types'] == 'ADAC'].copy()
+adata_ATAC = multiome[:, multiome.var['feature_types'] == 'ATAC'].copy()
 sc.pp.highly_variable_genes(
     adata_ATAC,
     n_top_genes=8000,
     subset=True
 )
-highly_variable_genes = adata_GEX.var_names
-highly_variable_features = adata_ATAC.var_names
-combined_mask = multiome.var_names.isin(highly_variable_genes) | multiome.var_names.isin(highly_variable_features)
-multiome = multiome[:, combined_mask]
+### TODO: debug, adata_ATAC contains 8006 peaks
+adata_ATAC = adata_ATAC[:, :8000]
+highly_variable_peaks_mask = adata_ATAC.var['highly_variable']
+highly_variable_genes_mask = adata_GEX.var['highly_variable']
+multiome = multiome[:, highly_variable_peaks_mask | highly_variable_genes_mask]
 print(multiome)
+sys.exit()
+#####################################################################################################################################
 
 X = multiome.X.toarray()
 X = torch.tensor(X).to(device)
-# X = X[:73511]   # Matrix is too large. Remove certain rows to save memory.
+X = X[:47025]   # Matrix is too large. Remove certain rows to save memory.
+### TODO: ground_truth values are repeated
 ground_truth = X.clone()
 
-print(len(np.where(multiome.obs['Site'] == 'site1')[0]))
-print(len(np.where(multiome.obs['Site'] == 'site2')[0]))
-print(len(np.where(multiome.obs['Site'] == 'site3')[0]))
-print(len(np.where(multiome.obs['Site'] == 'site4')[0]))
-sys.exit()
-
 mask = torch.zeros(X.shape, dtype=torch.bool).to(device)
-mask[41482:73511, 2000:] = True   # mask X(3,2)
+mask[-14556:, 8000:] = True   # mask X(3,2)
 
-nonzero_mask1222 = (X[:41482, 2000:] != 0).to(device)   # nonzero data of X(1,2), X(2,2)
-nonzero_mask31 = (X[-32029:, :2000] != 0).to(device)   # nonzero data of X(3,1)
-nonzero_mask32 = (X[-32029:, 2000:] != 0).to(device)   # nonzero data of X(3,2)
-mean_values = torch.sum(X[:41482, 2000:], dim=0) / torch.sum(nonzero_mask1222, dim=0)
-imps = mean_values.repeat(32029).to(device)
+nonzero_mask1222 = (X[:32469, 8000:] != 0).to(device)   # nonzero data of X(1,2), X(2,2)
+nonzero_mask31 = (X[-14556:, :8000] != 0).to(device)   # nonzero data of X(3,1)
+nonzero_mask32 = (X[-14556:, 8000:] != 0).to(device)   # nonzero data of X(3,2)
+print(ground_truth[-14556:, 8000:][nonzero_mask32])
+sys.exit()
+mean_values = torch.sum(X[:32469, 8000:], dim=0) / torch.sum(nonzero_mask1222, dim=0)
+imps = mean_values.repeat(14556).to(device)
 imps += torch.randn(imps.shape, device=device) * 0.1
 imps.requires_grad = True
 
@@ -85,13 +90,13 @@ for epoch in range(epochs):
     X_imputed[mask] = imps
 
     if epoch == 0:
-        pearson_corr = pearsonr(X_imputed[-32029:, 2000:][nonzero_mask32].detach().cpu().numpy(), ground_truth[-32029:, 2000:][nonzero_mask32].detach().cpu().numpy())[0]
+        pearson_corr = pearsonr(X_imputed[-14556:, 8000:][nonzero_mask32].detach().cpu().numpy(), ground_truth[-14556:, 8000:][nonzero_mask32].detach().cpu().numpy())[0]
         print(f"pearson: {pearson_corr:.4f}\n")
 
-    X12 = X_imputed[:41482, :]
-    X3  = X_imputed[-32029:, :]
-    GEX = torch.transpose(X_imputed[:, :2000], 0, 1)
-    ATAC = torch.transpose(X_imputed[:, 2000:], 0, 1)
+    X12 = X_imputed[:32469, :]
+    X3  = X_imputed[-14556:, :]
+    ATAC = torch.transpose(X_imputed[:, :8000], 0, 1)
+    GEX  = torch.transpose(X_imputed[:, 8000:], 0, 1)
     loss = 0.5 * ot.sliced_wasserstein_distance(X12, X3) + 0.5 * ot.sliced_wasserstein_distance(GEX, ATAC)
 
     optimizer.zero_grad()
@@ -103,6 +108,6 @@ for epoch in range(epochs):
     X_imputed[mask] = imps
 
     if (epoch + 1) % 200 == 0:
-        pearson_corr = pearsonr(X_imputed[-32029:, 2000:][nonzero_mask32].detach().cpu().numpy(), ground_truth[-32029:, 2000:][nonzero_mask32].detach().cpu().numpy())[0]
+        pearson_corr = pearsonr(X_imputed[-14556:, 8000:][nonzero_mask32].detach().cpu().numpy(), ground_truth[-14556:, 8000:][nonzero_mask32].detach().cpu().numpy())[0]
         # wandb.log({"Iteration": epoch + 1, "loss": loss, "pearson": pearson_corr})
         print(f"Iteration {epoch + 1}/{epochs}: loss: {loss.item():.4f}, pearson: {pearson_corr:.4f}\n")

multiome/X41.py
@@ -6,7 +6,7 @@ import ot
 import sys
 import anndata
 import scanpy as sc
-# import wandb
+import wandb
 from scipy.stats import pearsonr
 
 # wandb.init(
@@ -21,17 +21,17 @@ from scipy.stats import pearsonr
 epochs = 1000000
 device = 'cuda:0'
 
-multiome = anndata.read_h5ad("/workspace/ImputationOT/data/GSE194122_openproblems_neurips2021_multiome_BMMC_processed.h5ad.gz")
+multiome = anndata.read_h5ad("/workspace/ImputationOT/data/GSE194122_openproblems_neurips2021_multiome_BMMC_processed.h5ad")
 multiome.var_names_make_unique()
 
 ### preprocess
-### step 1
-adata_GEX = multiome[:, multiome.var['feature_types'] == 'GEX'].copy()
-adata_ATAC = multiome[:, multiome.var['feature_types'] == 'ATAC'].copy()
-sc.pp.normalize_total(adata_GEX, target_sum=1e4)
-sc.pp.normalize_total(adata_ATAC, target_sum=1e4)
-multiome.X[:, multiome.var['feature_types'] == 'GEX'] = adata_GEX.X
-multiome.X[:, multiome.var['feature_types'] == 'ATAC'] = adata_ATAC.X
+### step 1: normalize (skip)
+# adata_GEX = multiome[:, multiome.var['feature_types'] == 'GEX'].copy()
+# adata_ATAC = multiome[:, multiome.var['feature_types'] == 'ATAC'].copy()
+# sc.pp.normalize_total(adata_GEX, target_sum=1e4)
+# sc.pp.normalize_total(adata_ATAC, target_sum=1e4)
+# multiome.X[:, multiome.var['feature_types'] == 'GEX'] = adata_GEX.X
+# multiome.X[:, multiome.var['feature_types'] == 'ATAC'] = adata_ATAC.X
 ### step 2
 sc.pp.log1p(multiome)
 ### step 3
@@ -51,6 +51,8 @@ highly_variable_genes = adata_GEX.var_names
 highly_variable_features = adata_ATAC.var_names
 combined_mask = multiome.var_names.isin(highly_variable_genes) | multiome.var_names.isin(highly_variable_features)
 multiome = multiome[:, combined_mask]
+
+sys.exit()
 
 X = multiome.X.toarray()
 X = torch.tensor(X).to(device)