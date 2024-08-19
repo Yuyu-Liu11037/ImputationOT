a9fa43a283d59f25b321b3dad9af41941b6e608b
Eve Liu <eliu11037@gmail.com>
2024-08-19 12:46:19 -0400

    update 4gex.py

--- multiome/4gex.py
+++ multiome/4gex.py
@@ -38,10 +38,10 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 device = 'cuda:0'
-SITE1_CELL = 17243
-SITE2_CELL = 15226
-SITE3_CELL = 14556
-SITE4_CELL = 22224
+SITE1 = 17243
+SITE2 = 15226
+SITE3 = 14556
+SITE4 = 22224
 
 if args.use_wandb is True:
     wandb.init(
@@ -87,19 +87,19 @@ print(f"Finish preprocessing\n")
 #####################################################################################################################################
 
 X = multiome.layers['counts'].toarray()
-X3 = X[32469:47025].copy()
 X = torch.tensor(X).to(device)
-X = torch.cat((X[:32469], X[-SITE4_CELL:]), dim=0)   # Matrix is too large. Remove certain rows to save memory.
+X12 = X[:SITE1 + SITE2]
+X = X[SITE1 + SITE2:]   # Matrix is too large. Remove certain rows to save memory.
 ground_truth = X.clone()
 
 mask = torch.zeros(X.shape, dtype=torch.bool).to(device)
-mask[-22224:, num_atac:] = True   # mask X(4,2)
-nonzero_mask1222 = (X[:32469, num_atac:] != 0).to(device)   # nonzero data of X(1,2), X(2,2)
-nonzero_mask41 = (X[-22224:, :num_atac] != 0).to(device)   # nonzero data of X(4,1)
-nonzero_mask42 = (X[-22224:, num_atac:] != 0).to(device)   # nonzero data of X(4,2)
+mask[-SITE4:, num_atac:] = True   # mask X(4,2)
+nonzero_mask32 = (X[:SITE3, num_atac:] != 0).to(device)   # nonzero data of X(3,2)
+nonzero_mask41 = (X[-SITE4:, :num_atac] != 0).to(device)   # nonzero data of X(4,1)
+nonzero_mask42 = (X[-SITE4:, num_atac:] != 0).to(device)   # nonzero data of X(4,2)
 
-mean_values = torch.sum(X[:32469, num_atac:], dim=0) / torch.sum(nonzero_mask1222, dim=0)
-imps = mean_values.repeat(22224).to(device)
+mean_values = torch.sum(X[:SITE3, num_atac:], dim=0) / torch.sum(nonzero_mask32, dim=0)
+imps = mean_values.repeat(SITE4).to(device)
 imps += torch.randn(imps.shape, device=device) * 0.1
 imps.requires_grad = True
 
@@ -126,16 +126,16 @@ for epoch in range(args.epochs):
     X_imputed[mask] = imps
 
     if epoch == 0 and args.use_wandb is True:
-        pearson_corr = pearsonr(X_imputed[-22224:, num_atac:][nonzero_mask42].detach().cpu().numpy(), ground_truth[-22224:, num_atac:][nonzero_mask42].detach().cpu().numpy())[0]
-        multiome.X = np.vstack((X_imputed[:32469].detach().cpu().numpy(), X3, X_imputed[-22224:].detach().cpu().numpy()))
+        pearson_corr = pearsonr(X_imputed[-SITE4:, num_atac:][nonzero_mask42].detach().cpu().numpy(), ground_truth[-SITE4:, num_atac:][nonzero_mask42].detach().cpu().numpy())[0]
+        multiome.X = np.vstack((X12, X_imputed.detach().cpu().numpy()))
         ari, nmi = tools.cluster_with_kmeans(multiome, n_clusters=20, use_pca=False)
         print(f"Initial pearson: {pearson_corr:.4f}, ari: {ari:.4f}, nmi: {nmi:.4f}")
         wandb.log({"Iteration": 0, "loss": 0, "pearson": pearson_corr, "ari": ari, "nmi": nmi})
 
-    indices1 = torch.randperm(SITE1_CELL + SITE2_CELL, device=device)[:args.batch_size]
-    indices2 = torch.randperm(SITE4_CELL, device=device)[:args.batch_size]
-    X12 = X_imputed[:SITE1_CELL + SITE2_CELL][indices1]
-    X4  = X_imputed[-SITE4_CELL:][indices2]
+    indices1 = torch.randperm(SITE3, device=device)[:args.batch_size]
+    indices2 = torch.randperm(SITE4, device=device)[:args.batch_size]
+    X3 = X_imputed[:SITE3][indices1]
+    X4  = X_imputed[-SITE4:][indices2]
     ATAC = torch.transpose(X_imputed[:, :num_atac], 0, 1)
     GEX  = torch.transpose(X_imputed[:, num_atac:], 0, 1)
 
@@ -154,8 +154,8 @@ for epoch in range(args.epochs):
     
     w_h = 0 if epoch < args.start_aux else args.aux_weight
     omics_loss = SamplesLoss()(GEX, ATAC)
-    cells_loss = SamplesLoss()(X12, X4)
-    loss = 0.5 * 0.1 * omics_loss + 0.5 * cells_loss + w_h * h_loss
+    cells_loss = SamplesLoss()(X3, X4)
+    loss = 0.5 * 0.01 * omics_loss + 0.5 * 0.1 * cells_loss + w_h * h_loss
     print(f"{epoch}: omics_loss = {omics_loss.item():.4f}, cells_loss = {cells_loss.item():.4f}, h_loss = {h_loss.item():.4f}")
 
     optimizer.zero_grad()
@@ -167,8 +167,8 @@ for epoch in range(args.epochs):
         X_imputed = X.detach().clone()
         X_imputed[mask] = imps
         
-        pearson_corr = pearsonr(X_imputed[-22224:, num_atac:][nonzero_mask42].detach().cpu().numpy(), ground_truth[-22224:, num_atac:][nonzero_mask42].detach().cpu().numpy())[0]
-        multiome.X = np.vstack((X_imputed[:32469].detach().cpu().numpy(), X3, X_imputed[-22224:].detach().cpu().numpy()))
+        pearson_corr = pearsonr(X_imputed[-SITE4:, num_atac:][nonzero_mask42].detach().cpu().numpy(), ground_truth[-SITE4:, num_atac:][nonzero_mask42].detach().cpu().numpy())[0]
+        multiome.X = np.vstack((X12, X_imputed.detach().cpu().numpy()))
         ari, nmi = tools.cluster_with_leiden(multiome, resolution_values=[0.15, 0.20, 0.25])
         print(f"Iteration {epoch + 1}/{args.epochs}: loss: {loss.item():.4f}, pearson: {pearson_corr:.4f}, ari: {ari:.4f}, nmi: {nmi:.4f}")
         wandb.log({"Iteration": epoch + 1, "loss": loss, "pearson": pearson_corr, "ari": ari, "nmi": nmi})
