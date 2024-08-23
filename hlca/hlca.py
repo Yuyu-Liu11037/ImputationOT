42d191264f2a41b36ec849452d1a52ee10a932e0
Yuyu-Liu11037 <eliu11037@gmail.com>
2024-08-23 07:32:49 +0000

    add HLCA dataset

---
+++ hlca/hlca.py
@@ -0,0 +1,177 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import ot
+import anndata as ad
+import scanpy as sc
+import wandb
+import sys
+import random
+import argparse
+from scipy.stats import pearsonr
+from geomloss import SamplesLoss
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
+
+from utils import tools
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--use_wandb", action="store_true", default=False)
+parser.add_argument("--aux_weight", type=float, default=0)
+parser.add_argument("--epochs", type=int, default=150)
+parser.add_argument("--eval_interval", type=int, default=5)
+parser.add_argument("--start_aux", type=int, default=20)
+parser.add_argument("--batch_size", type=int, default=3000)
+parser.add_argument("--seed", type=int, default=2024)
+
+parser.add_argument("--source_batches", type=str, default="1", help="Impute batch range from 1 to 3")
+parser.add_argument("--target_batch", type=int, default=2, help="Batch number to impute")
+
+parser.add_argument("--wandb_group", type=str, default="hlca")
+parser.add_argument("--wandb_job", type=str, choices=["main", "ablation", "aux"], default="main")
+parser.add_argument("--wandb_name", type=str, default="exp")
+args = parser.parse_args()
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = 'cuda:0'
+batch_sizes = [7015, 1672, 752]
+batch_sizes_cumsum = np.cumsum([0] + batch_sizes)
+
+source_batches = list(map(int, args.source_batches.split(',')))
+target_batch = args.target_batch
+remaining_batches = [i for i in range(1, len(batch_sizes) + 1) if i not in source_batches and i != target_batch]
+
+if args.use_wandb is True:
+    wandb.init(
+        project="ot",
+        group=args.wandb_group,
+        job_type=args.wandb_job,
+        name=args.wandb_name,
+        config={
+            "dataset": "hlca",
+            "epochs": args.epochs,
+            "h_loss weight": args.aux_weight
+        }
+    )
+
+hlca = ad.read_h5ad("/workspace/ImputationOT/data/facs_normal_lung_blood_scanpy.20200205.RC4.h5ad") 
+hlca.var_names_make_unique()
+
+#####################################################################################################################################
+print("Start preprocessing")
+### preprocess
+### step 1: normalize
+sc.pp.normalize_total(hlca, target_sum=1e4)
+### step 2: log transform
+sc.pp.log1p(hlca)
+### step 3: select highly variable features
+sc.pp.highly_variable_genes(hlca, subset=True)
+print(f"Finish preprocessing\n")
+#####################################################################################################################################
+
+X = hlca.X
+X = torch.tensor(X).to(device)
+X_source = torch.cat([X[batch_sizes_cumsum[i - 1]:batch_sizes_cumsum[i]] for i in source_batches], dim=0)
+X_target = X[batch_sizes_cumsum[target_batch - 1]:batch_sizes_cumsum[target_batch]]
+X_del = torch.cat([X[batch_sizes_cumsum[i-1]:batch_sizes_cumsum[i]] for i in remaining_batches], dim=0) 
+
+ground_truth = X_target.clone()
+
+mask = torch.zeros(X_target.shape, dtype=torch.bool).to(device)
+nonzero_mask_source = (X_source != 0).to(device)
+nonzero_mask_target = (X_target != 0).to(device) 
+
+epsilon = 1e-8
+mean_values = torch.sum(X_source * nonzero_mask_source, dim=0) / (torch.sum(nonzero_mask_source, dim=0) + epsilon)
+imps = mean_values.repeat(X_target.shape[0]).to(device)
+imps += torch.randn(imps.shape, device=device) * 0.1
+imps.requires_grad = True
+imps = imps.view(X_target.shape).detach().requires_grad_()
+
+### best performance
+def lr_lambda(epoch):
+    if epoch < 20:
+        return 1.0
+    elif 20 <= epoch < 100:
+        return 1.1 - (epoch - 20) / 80.0
+    elif 100 <= epoch < 1000:
+        return 0.101 - (epoch - 100) / 9000.0
+    else:
+        return 0.001
+
+optimizer = optim.Adam([imps], 1.0)
+scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+
+h_loss = torch.zeros(1).to(device)
+cells_loss = torch.zeros(1).to(device)
+
+print(f"Start optimizing: use batch(es) {args.source_batches} to impute batch {args.target_batch}")
+for epoch in range(args.epochs):
+    X_imputed = imps
+
+    if epoch == 0 and args.use_wandb is True:
+        pearson_corr = pearsonr(X_imputed[nonzero_mask_target].detach().cpu().numpy(), ground_truth[nonzero_mask_target].detach().cpu().numpy())[0]
+        X_full = []
+        for i in range(1, len(batch_sizes) + 1):
+            if i in source_batches:
+                tmp = X[batch_sizes_cumsum[i-1]:batch_sizes_cumsum[i]].detach().cpu().numpy()
+            elif i == target_batch:
+                tmp = X_imputed.detach().cpu().numpy()
+            else:
+                tmp = X[batch_sizes_cumsum[i-1]:batch_sizes_cumsum[i]].detach().cpu().numpy()
+            X_full.append(tmp)
+        hlca.X = np.vstack(X_full)
+        ari, nmi = tools.cluster_with_kmeans(hlca, n_clusters=20, use_pca=False)
+        print(f"Initial pearson: {pearson_corr:.4f}, ari: {ari:.4f}, nmi: {nmi:.4f}")
+        wandb.log({"Iteration": 0, "loss": 0, "pearson": pearson_corr, "ari": ari, "nmi": nmi})
+
+    indices1 = torch.randperm(X_source.shape[0], device=device)[:args.batch_size]
+    indices2 = torch.randperm(X_target.shape[0], device=device)[:args.batch_size]
+    X1 = X_source[indices1]
+    X2 = X_imputed[indices2]
+
+    if epoch >= args.start_aux:
+        ### calculate cluster results
+        labels1 = tools.calculate_cluster_labels(X1, resolution=0.9)
+        labels2 = tools.calculate_cluster_labels(X2, resolution=0.9)
+        ### calculate cluster centroids
+        centroids1 = tools.calculate_cluster_centroids(X1, labels1)
+        centroids2 = tools.calculate_cluster_centroids(X2, labels2)
+        ### calculate cluster loss
+        h_loss = SamplesLoss()(centroids1, centroids2)
+    
+    w_h = 0 if epoch < args.start_aux else args.aux_weight
+    cells_loss = SamplesLoss()(X1, X2)
+    loss = cells_loss + w_h * h_loss
+    lr = lr_lambda(epoch)
+    print(f"{epoch}: lr = {lr:.4f}, cells_loss = {cells_loss.item():.4f}, h_loss = {h_loss.item():.4f}")
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+
+    if (epoch + 1) % args.eval_interval == 0 and args.use_wandb is True:
+        X_imputed = imps
+        
+        pearson_corr = pearsonr(X_imputed[nonzero_mask_target].detach().cpu().numpy(), ground_truth[nonzero_mask_target].detach().cpu().numpy())[0]
+        X_full = []
+        for i in range(1, len(batch_sizes) + 1):
+            if i in source_batches:
+                tmp = X[batch_sizes_cumsum[i-1]:batch_sizes_cumsum[i]].detach().cpu().numpy()
+            elif i == target_batch:
+                tmp = X_imputed.detach().cpu().numpy()
+            else:
+                tmp = X[batch_sizes_cumsum[i-1]:batch_sizes_cumsum[i]].detach().cpu().numpy()
+            X_full.append(tmp)
+        hlca.X = np.vstack(X_full)
+        ari, nmi = tools.cluster_with_leiden(hlca, resolution_values=[0.85, 0.90])
+        print(f"Iteration {epoch + 1}/{args.epochs}: loss: {loss.item():.4f}, pearson: {pearson_corr:.4f}, ari: {ari:.4f}, nmi: {nmi:.4f}")
+        wandb.log({"Iteration": epoch + 1, "loss": loss, "pearson": pearson_corr, "ari": ari, "nmi": nmi})
--- hlca/utils/tools.py
+++ hlca/utils/tools.py
@@ -3,13 +3,39 @@ import anndata as ad
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 
 
-def clustering(adata, resolution_values=[0.10, 0.15, 0.20, 0.25]):
+random_seed = 2024
+np.random.seed(random_seed)
+torch.manual_seed(random_seed)
+sc.settings.seed = random_seed
+torch.cuda.manual_seed(random_seed)
+
+    
+def cluster_with_kmeans(adata, n_clusters=10, use_pca=True, n_pcs=50):
+    data = adata.X
+
+    if use_pca:
+        sc.tl.pca(adata, n_comps=n_pcs)
+        data = adata.obsm['X_pca']
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+    true_labels = adata.obs["Annotation"]
+    predicted_labels = kmeans.fit_predict(data).astype(str)
+    
+    ari = adjusted_rand_score(true_labels, predicted_labels)
+    nmi = normalized_mutual_info_score(true_labels, predicted_labels)
+
+    return ari, nmi
+
+
+def cluster_with_leiden(adata, resolution_values=[0.10, 0.20, 0.30, 0.40]):
     sc.pp.pca(adata)
     sc.pp.neighbors(adata, use_rep="X_pca")
-    true_labels = adata.obs["cell_type"]
+    true_labels = adata.obs["Annotation"]
     best_ari, best_nmi = 0, 0
 
     for resolution in resolution_values:
@@ -34,11 +60,11 @@ def gumbel_sinkhorn(X, tau=1.0, n_iter=20, epsilon=1e-6):
     return X
 
 
-def calculate_cluster_labels(X):
+def calculate_cluster_labels(X, resolution=0.65):
     adata = ad.AnnData(X.detach().cpu().numpy())
     sc.pp.pca(adata)
     sc.pp.neighbors(adata, use_rep="X_pca")
-    sc.tl.leiden(adata, resolution=0.2, flavor="igraph", n_iterations=2)
+    sc.tl.leiden(adata, resolution=resolution, flavor="igraph", n_iterations=2)
     predicted_labels = adata.obs["leiden"]
     cluster_labels = torch.tensor(predicted_labels.astype(int).values)
     return cluster_labels
@@ -51,38 +77,3 @@ def calculate_cluster_centroids(X, cluster_labels):
         centroids.append(cluster_centroid)
     centroids = torch.stack(centroids)
     return centroids
-
-def preprocess(adata, omics1="GEX", omics2="ADT", target_sum=1e4, layer=None, n_filter1=None):
-    """
-    :param adata: dataset name
-    :param omics1: GEX or ATAC
-    :param omics2: ADT or GEX
-    :param target_sum: default 1e4
-    :param layer: None or counts
-    :param n_filter1: 2000 or 4000
-    :return:
-    """
-    ### preprocess
-    adata_omics1 = adata[:, adata.var["feature_types"] == omics1].copy()
-    adata_omics2 = adata[:, adata.var["feature_types"] == omics2].copy()
-    ### step 1
-    sc.pp.normalize_total(adata_omics1, target_sum=target_sum, layer=layer)
-    sc.pp.normalize_total(adata_omics2, target_sum=target_sum, layer=layer)
-    ### step 2
-    sc.pp.log1p(adata_omics1, layer=layer)
-    sc.pp.log1p(adata_omics2, layer=layer)
-    ### step 3
-    sc.pp.highly_variable_genes(
-        adata_omics1,
-        n_top_genes=n_filter1,
-        subset=True,
-        layer=layer
-    )
-    if omics2 == "GEX":
-        sc.pp.highly_variable_genes(
-            adata_omics2,
-            subset=True,
-            layer=layer
-        )
-    adata = ad.concat([adata_omics1, adata_omics2], axis=1, merge="first")
-    return adata
